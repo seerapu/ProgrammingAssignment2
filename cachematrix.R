1cf26058c81ae7f3008bd6f8be8e2d1c6f975cb6
seerapu <ssreddy75@gmail.com>
2014-05-25 15:04:51 -0700

    Modified the file cachematirx.R as per the assignment task.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Following functions creates a special matrix that can cache its inverse. 
+# But if the inverse already exists then it returns the value from the cache.
+# 
+# Usage:
+# > x <- matrix(rnorm(4), nrow = 2)         
+# > zx <- makeCacheMatrix(x)            
+# > zx$get()                     
+# > cacheSolve(zx)                       
+# > cacheSolve(zx)   // Second time to return the cached inverse                      
+                                          
 
-## Write a short comment describing this function
+
+# makeCacheMatrix: return a list of functions to:
+# 1. Set the value of the matrix
+# 2. Get the value of the matrix
+# 3. Set the value of the inverse
+# 4. Get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        invm <- NULL
+        set <- function(y) {
+                x <<- y
+                invm <<- NULL
+        }
+        get <- function() x
+        setinvm <- function(inverse) invm <<- inverse
+        getinvm <- function() invm
+        list(set = set, get = get,
+             setinvm = setinvm,
+             getinvm = getinvm)        
 }
 
 
-## Write a short comment describing this function
+# cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+# If the inverse has already been calculated (and the matrix has not changed), then the cachesolve retrieves the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        invm <- x$getinvm()
+        if(!is.null(invm)) {
+                message("getting cached data")
+                return(invm)
+        }
+        data <- x$get()
+        invm <- solve(data, ...)
+        x$setinvm(invm)
+        invm
 }
+
